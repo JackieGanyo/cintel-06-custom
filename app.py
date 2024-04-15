2af9f50d63af084ea4fe7685536b031f99c6be69
Jacquelyn Ganyo <162255714+JackieGanyo@users.noreply.github.com>
2024-04-14 22:36:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-# Import helpers for plotting logic
-from plots import color_palette, density_plot, radar_chart
 
-# Import some pre-downloaded data on player careers
-from shared import app_dir, careers_df, from_start, gp_max, players_dict, stats, to_end
+# Import data on player careers
+import nhlstatsapi
 from shiny import reactive, req
 from shiny.express import input, ui
 from shinywidgets import render_plotly
